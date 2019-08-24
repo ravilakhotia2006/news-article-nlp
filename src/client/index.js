0d40fa7201aaa0aa8a062c8d456914184d3d42e6
ravilakhotia <ravilakhotia2006@gmail.com>
2019-08-24 11:49:12 +0530

    first commit

---
+++ src/client/index.js
@@ -0,0 +1,8 @@
+import "./styles/base.scss";
+import "./styles/footer.scss";
+import "./styles/form.scss";
+import "./styles/header.scss";
+import "./styles/main.scss";
+
+export * from "./js/checkStatement";
+export * from "./js/checkArticle";
---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+const path = require("path");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const WorkboxPlugin = require("workbox-webpack-plugin");
+
+module.exports = {
+  devServer: {
+    host: "localhost",
+    port: "3000",
+    proxy: [
+      {
+        context: ["/checkArticle", "/checkStatement"],
+        target: "http://localhost:8000"
+      }
+    ],
+    hot: true,
+    overlay: true,
+    headers: {
+      "Access-Control-Allow-Origin": "*"
+    },
+    historyApiFallback: true
+  },
+  entry: "./src/client/index.js",
+  output: {
+    path: path.join(__dirname, "dist"),
+    filename: "bundle-[hash].min.js",
+    libraryTarget: "var",
+    library: "Client"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader"
+      },
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"]
+      }
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin({ verbose: true }),
+    new HtmlWebPackPlugin({
+      template: "./src/client/views/index.html",
+      hash: true,
+      xhtml: true
+    }),
+    new WorkboxPlugin.GenerateSW()
+  ]
+};
