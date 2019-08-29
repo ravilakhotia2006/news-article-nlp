2e4321b08c26695131dcb0f31b87a4a475cb5549
ravilakhotia <ravilakhotia2006@gmail.com>
2019-08-29 22:16:10 +0530

    deleted all the files

--- src/client/index.js
+++
@@ -1,8 +0,0 @@
-import "./styles/base.scss";
-import "./styles/footer.scss";
-import "./styles/form.scss";
-import "./styles/header.scss";
-import "./styles/main.scss";
-
-export * from "./js/checkStatement";
-export * from "./js/checkArticle";
